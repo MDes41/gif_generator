523608e0c54e728440f249736b198c8d75c5a797
Matt DesMarteau <MDes41@users.noreply.github.com>
2016-09-11 18:59:59 -0600

    Fixed password fields
    - password fields were not hiding the letters, so they have been
    changed to password fields

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,8 +7,9 @@ class SessionsController < ApplicationController
     @user = User.find_by(username: params[:session][:username])
     if @user && @user.authenticate(params[:session][:password])
       session[:user_id] = @user.id
-      redirect_to @user
+      redirect_to admin_categories_path
     else
+      render :new
     end
   end
 end
